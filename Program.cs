2dd20026d2957fd9700c951fee9d440d635fe5b8
Maxime ANDRE <maxime.andre1986@gmail.com>
2024-05-07 15:04:41 +0200

    Added a thread

--- Program.cs
+++ Program.cs
@@ -6,59 +6,131 @@ using System.Text.Json.Serialization;
 
 public class MPF
 {
+    private const string expectedEvent = "slide_slide4_created";
     private TcpClient? client = null;
+    private bool quit = false;
+    private Mutex quitMutex = new Mutex(false);
+    private bool startGame = false;
+    private Mutex startGameMutex = new Mutex(false);
+    Thread thread;
+    bool firstLoop = true;
 
-    public bool Connect()
+    public MPF()
     {
-        try
-        {
-            client = new TcpClient();
-            client.Connect("127.0.0.1", 5051);
-            Stream strm = client.GetStream();
-            StreamReader reader = new(strm, Encoding.ASCII);
-            StreamWriter writer = new(strm, Encoding.ASCII);
+        thread = new Thread(new ThreadStart(ThreadProc));
+        thread.Start();
+    }
+    ~MPF()
+    {
+        quitMutex.Dispose();
+        startGameMutex.Dispose();
+    }
 
-            string? str = reader.ReadLine();
-            if(str == null)
+    public void Stop()
+    {
+        quitMutex.WaitOne();
+        quit = true;
+        quitMutex.ReleaseMutex();
+    }
+
+    private void Pause()
+    {
+        int seconds = 10;
+        Console.WriteLine($"Waiting for {seconds} seconds...");
+        Thread.Sleep(seconds * 1000);
+    }
+
+    private void ThreadProc()
+    {
+        while (true)
+        {
+            quitMutex.WaitOne();
+            bool q = quit;
+            quitMutex.ReleaseMutex();
+            if (q)
             {
-                reader.Close();
-                writer.Close();
-                client.Close();
+                break;
             }
-            str = str?.Split('&')[0];
-            Console.WriteLine(str);
-            writer.WriteLine(str);
-            writer.WriteLine("monitor_start?category=events");
-            writer.Flush();
-            return true;
 
-        } catch (Exception e)
+            if(firstLoop) firstLoop = false; else Pause();
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect("127.0.0.1", 5051);
+                Stream strm = client.GetStream();
+                StreamReader reader = new(strm, Encoding.ASCII);
+                StreamWriter writer = new(strm, Encoding.ASCII);
+                string? str = reader.ReadLine();
+                if (str == null)
+                {
+                    reader.Close();
+                    writer.Close();
+                    client.Close();
+                    continue;
+                }
+                str = str?.Split('&')[0];
+                Console.WriteLine(str);
+                writer.WriteLine(str);
+                writer.WriteLine("monitor_start?category=events");
+                writer.Flush();
+                //writer.Dispose();
+                ReadLoop(reader);
+
+            }
+            catch (Exception e)
+            {
+                client?.Close();
+                client = null;
+            }
+        }
+        client?.Close();
+    }
+
+    public void ReadLoop(StreamReader reader)
+    {
+        string expectedPrefix = "monitored_event?json=";
+        while (true)
         {
-            client?.Close();
-            client = null;
-            return false;
+            string? str = reader.ReadLine();
+            if (str == null)
+                return;
+            string prefix = str.Substring(0, expectedPrefix.Length);
+            if (prefix == expectedPrefix)
+            {
+                string json = str.Substring(expectedPrefix.Length);
+                JsonNode? ev = JsonNode.Parse(json);
+                string? ev_name = (string)ev!["event_name"];
+                if (ev_name == null)
+                    continue;
+                Console.WriteLine($"event: {ev_name}");
+                if(ev_name == expectedEvent)
+                {
+                    startGameMutex.WaitOne();
+                    startGame = true;
+                    startGameMutex.ReleaseMutex();
+                }
+            }
         }
     }
 
-    public void Read()
+    public void WaitForEvent()
     {
-        Stream strm = client?.GetStream();
-        StreamReader reader = new(strm, Encoding.ASCII);
-        string? str = reader.ReadLine();
-        string expectedPrefix = "monitored_event?json=";
-        string prefix = str.Substring(0, expectedPrefix.Length);
-        if(prefix == expectedPrefix)
+        while (true)
         {
-            string json = str.Substring(expectedPrefix.Length);
-            JsonNode? ev = JsonNode.Parse(json);
-            string ev_name = (string)ev!["event_name"];
-            Console.WriteLine($"event: {ev_name}");
+            startGameMutex.WaitOne();
+            bool start = startGame;
+            startGameMutex.ReleaseMutex();
+            if (start) return; else Thread.Sleep(10);
         }
     }
 
     public void Close()
     {
         client?.Close();
+        quitMutex.WaitOne();
+        quit = true;
+        quitMutex.ReleaseMutex();
     }
 }
 
@@ -67,8 +139,8 @@ public class MPFTest
     public static void Main()
     {
         MPF mpf = new MPF();
-        mpf.Connect();
-        while(true) {  mpf.Read(); }
+        mpf.WaitForEvent();
+        Console.WriteLine("Starting game");
         mpf.Close();
     }
 }
